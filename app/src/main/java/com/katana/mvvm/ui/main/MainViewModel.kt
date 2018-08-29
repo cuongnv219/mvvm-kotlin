40eb660fa338f98dd28818f43fb1f020d36e49cb
cuongnv <cuongnv219@gmail.com>
2018-08-29 08:49:28 +0700

    Update kotlin version

--- app/src/main/java/com/katana/mvvm/ui/main/MainViewModel.kt
+++ app/src/main/java/com/katana/mvvm/ui/main/MainViewModel.kt
@@ -1,13 +1,13 @@
 package com.katana.mvvm.ui.main
 
 import com.katana.mvvm.base.BaseViewModel
-import com.katana.mvvm.data.AppDataManager
+import com.katana.mvvm.data.DataManager
 import com.utils.SchedulerProvider
 
 /**
  * Created by Kaz on 09:59 8/20/18
  */
-class MainViewModel(dataManager: AppDataManager, schedulerProvider: SchedulerProvider) : BaseViewModel<MainNavigator>(
+class MainViewModel(dataManager: DataManager, schedulerProvider: SchedulerProvider) : BaseViewModel<MainNavigator>(
         dataManager, schedulerProvider) {
 
     fun doSomething() {
